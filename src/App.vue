f069fcfcf5fc6289acece43049d66893f87c1b04
guilhermelpl <guilopes1003@gmail.com>
2024-08-18 15:23:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import api from './utils/api';
+
+const todos = ref([]);
+
+onMounted(() => {
+  api.get('/todos').then((response) => {
+    todos.value = response.data;
+  });
+});
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
